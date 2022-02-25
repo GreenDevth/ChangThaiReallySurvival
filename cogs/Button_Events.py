d15638128c0c67e978f57233a13eb27bb0f5bf38
GreenDevth <theerapong@rumail.ru.ac.th>
2022-02-25 20:00:16 +0700

    edit button

--- cogs/Button_Events.py
+++ cogs/Button_Events.py
@@ -1,8 +1,9 @@
 import asyncio
+
 import discord
 from discord.ext import commands
-from discord_components import Button, ButtonStyle
-from players.players_db import players_exists, update_steam_id
+
+from players.players_db import update_steam_id
 
 
 class ButtonEvent(commands.Cog):
@@ -15,50 +16,47 @@ class ButtonEvent(commands.Cog):
         server_btn = interaction.component.custom_id
 
         if server_btn == 'new_players':
-            check = players_exists(member.id)
-            if check == 1:
-                await interaction.respond(content='📝 กรุณาพิมพ์ steam id ของคุณที่นี่ เพื่อลงทะเบียน'
-                                                  '\nPlease enter your steam id to register. ')
-                while True:
-                    try:
-                        msg = await self.bot.wait_for(
-                            'message',
-                            check=lambda r: r.author == interaction.author and r.channel == interaction.channel,
-                            timeout=300
-                        )
-                        if msg.content.isdigit():
-                            a_string = str(msg.content)
-                            length = len(a_string)
+            await interaction.respond(content='📝 กรุณาพิมพ์ steam id ของคุณที่นี่ เพื่อลงทะเบียน'
+                                              '\nPlease enter your steam id to register. ')
+            while True:
+                try:
+                    msg = await self.bot.wait_for(
+                        'message',
+                        check=lambda r: r.author == interaction.author and r.channel == interaction.channel,
+                        timeout=300
+                    )
+                    if msg.content.isdigit():
+                        a_string = str(msg.content)
+                        length = len(a_string)
 
-                            discord_name = str(member.name)
-                            discord_id = str(member.id)
-                            convert = discord_id[:5]
-                            bank_id = str(convert)
+                        discord_name = str(member.name)
+                        discord_id = str(member.id)
+                        convert = discord_id[:5]
+                        bank_id = str(convert)
 
-                            if length == 17:
-                                update_steam_id(discord_id, msg.content)
-                                await discord.DMChannel.send(
-                                    member,
-                                    "🎉 ยินดีต้อนรับอย่างเป็นทางการสู่สังคม ChangThai℠ Really survival "
-                                )
-                                verify = discord.utils.get(interaction.guild.roles, name='Verify Members')
-                                role = discord.utils.get(interaction.guild.roles, name='joiner')
-                                await member.add_roles(verify)
-                                await member.remove_roles(role)
-                                await msg.delete()
-                                return
-
-                        else:
-                            await interaction.channel.send(
-                                f'{member.mention} : 📢 รูปแบบสตรีมไอดีของคุณไม่ถูกต้องกรุณาลองใหม่อีกครั้ง',
-                                delete_after=5)
+                        if length == 17:
+                            update_steam_id(discord_id, msg.content)
+                            await discord.DMChannel.send(
+                                member,
+                                "🎉 ยินดีต้อนรับอย่างเป็นทางการสู่สังคม ChangThai℠ Really survival "
+                            )
+                            verify = discord.utils.get(interaction.guild.roles, name='Verify Members')
+                            role = discord.utils.get(interaction.guild.roles, name='joiner')
+                            await member.add_roles(verify)
+                            await member.remove_roles(role)
                             await msg.delete()
+                            return
 
-                    except asyncio.TimeoutError:
-                        await interaction.send(
-                            f'{member.mention} : 📢 คุณใช้เวลาในการลงทะเบียนนานเกินไป กรุณาลงทะเบียนใหม่อีกครั้ง'
-                        )
+                    else:
+                        await interaction.channel.send(
+                            f'{member.mention} : 📢 รูปแบบสตรีมไอดีของคุณไม่ถูกต้องกรุณาลองใหม่อีกครั้ง',
+                            delete_after=5)
+                        await msg.delete()
 
+                except asyncio.TimeoutError:
+                    await interaction.send(
+                        f'{member.mention} : 📢 คุณใช้เวลาในการลงทะเบียนนานเกินไป กรุณาลงทะเบียนใหม่อีกครั้ง'
+                    )
 
 
 def setup(bot):
