831d4c352241a8d6b1f48df5acc91acf9f8415d7
GreenDevth <theerapong@rumail.ru.ac.th>
2022-02-27 14:49:43 +0700

    edit vote

--- cogs/Vote.py
+++ cogs/Vote.py
@@ -15,32 +15,10 @@ class VoteSystemComands(commands.Cog):
             await interation.edit_origin(
                 components=[
                     [
-                        Button(style=ButtonStyle.blue, label=f'TRACTOR {count + 1}', emoji='🚘', custom_id='tractor'),
-                        Button(style=ButtonStyle.gray, label=f'PICKUP {count}', emoji='🚘', custom_id='pickup_hell'),
-                        Button(style=ButtonStyle.red, label=f'BIKE {count}', emoji='🚘', custom_id='bike_hell'),
-                        Button(style=ButtonStyle.green, label=f'BYCICLE {count}', emoji='🚘', custom_id='bycicle')
-                    ]
-                ]
-            )
-        elif racing == 'pickup_hell':
-            await interation.edit_origin(
-                components=[
-                    [
-                        Button(style=ButtonStyle.blue, label=f'TRACTOR {count}', emoji='🚘', custom_id='tractor'),
-                        Button(style=ButtonStyle.gray, label=f'PICKUP {count + 1}', emoji='🚘', custom_id='pickup_hell'),
-                        Button(style=ButtonStyle.red, label=f'BIKE {count}', emoji='🚘', custom_id='bike_hell'),
-                        Button(style=ButtonStyle.green, label=f'BYCICLE {count}', emoji='🚘', custom_id='bycicle')
-                    ]
-                ]
-            )
-        elif racing == 'bike_hell':
-            await interation.edit_origin(
-                components=[
-                    [
-                        Button(style=ButtonStyle.blue, label=f'TRACTOR {count}', emoji='🚘', custom_id='tractor'),
-                        Button(style=ButtonStyle.gray, label=f'PICKUP {count}', emoji='🚘', custom_id='pickup_hell'),
-                        Button(style=ButtonStyle.red, label=f'BIKE {count + 1}', emoji='🚘', custom_id='bike_hell'),
-                        Button(style=ButtonStyle.green, label=f'BYCICLE {count}', emoji='🚘', custom_id='bycicle')
+                        Button(style=ButtonStyle.blue, label=f'TRACTOR : {count + 1}', custom_id='tractor'),
+                        Button(style=ButtonStyle.gray, label=f'BYCICLE : {count}', custom_id='bycicle'),
+                        Button(style=ButtonStyle.red, label=f'BIKE : {count}', custom_id='bike_hell'),
+                        Button(style=ButtonStyle.green, label=f'SUV : {count}', custom_id='suv')
                     ]
                 ]
             )
@@ -48,10 +26,32 @@ class VoteSystemComands(commands.Cog):
             await interation.edit_origin(
                 components=[
                     [
-                        Button(style=ButtonStyle.blue, label=f'TRACTOR {count}', emoji='🚘', custom_id='tractor'),
-                        Button(style=ButtonStyle.gray, label=f'PICKUP {count}', emoji='🚘', custom_id='pickup_hell'),
-                        Button(style=ButtonStyle.red, label=f'BIKE {count}', emoji='🚘', custom_id='bike_hell'),
-                        Button(style=ButtonStyle.green, label=f'BYCICLE {count + 1}', emoji='🚘', custom_id='bycicle')
+                        Button(style=ButtonStyle.blue, label=f'TRACTOR : {count}', custom_id='tractor'),
+                        Button(style=ButtonStyle.gray, label=f'BYCICLE : {count + 1}', custom_id='bycicle'),
+                        Button(style=ButtonStyle.red, label=f'BIKE : {count}', custom_id='bike_hell'),
+                        Button(style=ButtonStyle.green, label=f'SUV : {count}', custom_id='suv')
+                    ]
+                ]
+            )
+        elif racing == 'bike_hell':
+            await interation.edit_origin(
+                components=[
+                    [
+                        Button(style=ButtonStyle.blue, label=f'TRACTOR : {count}', custom_id='tractor'),
+                        Button(style=ButtonStyle.gray, label=f'BYCICLE : {count}', custom_id='bycicle'),
+                        Button(style=ButtonStyle.red, label=f'BIKE : {count + 1}', custom_id='bike_hell'),
+                        Button(style=ButtonStyle.green, label=f'SUV : {count}', custom_id='suv')
+                    ]
+                ]
+            )
+        elif racing == 'suv':
+            await interation.edit_origin(
+                components=[
+                    [
+                        Button(style=ButtonStyle.blue, label=f'TRACTOR : {count}', custom_id='tractor'),
+                        Button(style=ButtonStyle.gray, label=f'BYCICLE : {count}', custom_id='bycicle'),
+                        Button(style=ButtonStyle.red, label=f'BIKE : {count}', custom_id='bike_hell'),
+                        Button(style=ButtonStyle.green, label=f'SUV : {count + 1}', custom_id='suv')
                     ]
                 ]
             )
@@ -60,16 +60,17 @@ class VoteSystemComands(commands.Cog):
     @commands.command(name='racing')
     async def racing_command(self, ctx):
         await ctx.send(
-            file=discord.File('./img/event/tractor.png'),
+            file=discord.File('./img/event/tractor.jpg'),
             components=[
                 [
-                    Button(style=ButtonStyle.blue, label='TRACTOR : 0', emoji='🚘', custom_id='tractor'),
-                    Button(style=ButtonStyle.gray, label='PICKUP : 0', emoji='🚘', custom_id='pickup_hell'),
-                    Button(style=ButtonStyle.red, label='BIKE : 0', emoji='🚘', custom_id='bike_hell'),
-                    Button(style=ButtonStyle.green, label='BYCICLE : 0', emoji='🚘', custom_id='bycicle')
+                    Button(style=ButtonStyle.blue, label='TRACTOR : 0', custom_id='tractor'),
+                    Button(style=ButtonStyle.gray, label='BYCICLE : 0', custom_id='bycicle'),
+                    Button(style=ButtonStyle.red, label='BIKE : 0', custom_id='bike_hell'),
+                    Button(style=ButtonStyle.green, label='SUV : 0', custom_id='SUV')
                 ]
             ]
         )
+        await ctx.message.delete()
 
 
 def setup(bot):
