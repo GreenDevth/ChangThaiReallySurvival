81654e27a68aa2da243a71817c3dc3d70ec49d4f
GreenDevth <theerapong@rumail.ru.ac.th>
2022-02-27 21:09:17 +0700

    update file and create new event file

--- cogs/The_Racing.py
+++ cogs/The_Racing.py
@@ -21,13 +21,13 @@ class RacingEvent(commands.Cog):
                 message = "คุณได้ลงทะเบียนเป็นที่เรียบร้อย"
                 steam_id = get_steam_id(member.id)
                 new_players_event(member.name, member.id, steam_id)
-                
+
                 await interaction.edit_origin(
                     components=[
                         [
                             Button(style=ButtonStyle.blue, label='REGISTER', emoji='📝', custom_id='racing_register'),
                             Button(style=ButtonStyle.green, label=f'NUMBER OF REGISTERED PLAYERS : {total}', emoji='📝',
-                                custom_id='racing_count', disabled=False),
+                                   custom_id='racing_count', disabled=False),
                         ]
                     ]
                 )
@@ -39,28 +39,29 @@ class RacingEvent(commands.Cog):
                         [
                             Button(style=ButtonStyle.blue, label='REGISTER', emoji='📝', custom_id='racing_register'),
                             Button(style=ButtonStyle.green, label=f'NUMBER OF REGISTERED PLAYERS : {total}', emoji='📝',
-                                custom_id='racing_count', disabled=False),
+                                   custom_id='racing_count', disabled=False),
                         ]
                     ]
                 )
                 await interaction.channel.send("📢 คุณได้ลงทะเบียนไว้แล้ว", delete_after=5)
                 return
-        
+
         elif event_btn == "racing_count":
             await interaction.edit_origin(
-                    components=[
-                        [
-                            Button(style=ButtonStyle.blue, label='REGISTER', emoji='📝', custom_id='racing_register'),
-                            Button(style=ButtonStyle.green, label=f'NUMBER OF REGISTERED PLAYERS : {total}', emoji='📝',
-                                custom_id='racing_count', disabled=False),
-                        ]
+                components=[
+                    [
+                        Button(style=ButtonStyle.blue, label='REGISTER', emoji='📝', custom_id='racing_register'),
+                        Button(style=ButtonStyle.green, label=f'NUMBER OF REGISTERED PLAYERS : {total}', emoji='📝',
+                               custom_id='racing_count', disabled=False),
                     ]
-                )
+                ]
+            )
+
     @commands.command(name='racing')
     async def racing_command(self, ctx):
         total = count_event_player()
         await ctx.send(
-            file=discord.File('./img/event/bike2.png'),
+            file=discord.File('./img/event/the_racing.png'),
             components=[
                 [
                     Button(style=ButtonStyle.blue, label='REGISTER', emoji='📝', custom_id='racing_register'),
