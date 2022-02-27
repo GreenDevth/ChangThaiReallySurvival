6887bd527834b430dce15932250e4660d8ddfe66
GreenDevth <theerapong@rumail.ru.ac.th>
2022-02-27 21:07:24 +0700

    edit regitser event

--- cogs/The_Racing.py
+++ cogs/The_Racing.py
@@ -13,23 +13,49 @@ class RacingEvent(commands.Cog):
         member = interaction.author
         event_btn = interaction.component.custom_id
         total = count_event_player()
+        print(total)
+        check = players_exists(member.id)
         message = None
         if event_btn == "racing_register":
-            message = "คุณได้ลงทะเบียนเป็นที่เรียบร้อย"
-            steam_id = get_steam_id(member.id)
-            new_players_event(member.name, member.id, steam_id)
-            await interaction.edit_origin(
-                components=[
-                    [
-                        Button(style=ButtonStyle.blue, label='REGISTER', emoji='📝', custom_id='racing_register'),
-                        Button(style=ButtonStyle.red, label=f'NUMBER OF REGISTERED PLAYERS : {total}', emoji='📝',
-                               custom_id='racing_count', disabled=True),
+            if check == 0:
+                message = "คุณได้ลงทะเบียนเป็นที่เรียบร้อย"
+                steam_id = get_steam_id(member.id)
+                new_players_event(member.name, member.id, steam_id)
+                
+                await interaction.edit_origin(
+                    components=[
+                        [
+                            Button(style=ButtonStyle.blue, label='REGISTER', emoji='📝', custom_id='racing_register'),
+                            Button(style=ButtonStyle.green, label=f'NUMBER OF REGISTERED PLAYERS : {total}', emoji='📝',
+                                custom_id='racing_count', disabled=False),
+                        ]
                     ]
-                ]
-            )
-            await interaction.channel.send(message, delete_after=5)
-            return
-
+                )
+                await interaction.channel.send(message, delete_after=5)
+                return
+            else:
+                await interaction.edit_origin(
+                    components=[
+                        [
+                            Button(style=ButtonStyle.blue, label='REGISTER', emoji='📝', custom_id='racing_register'),
+                            Button(style=ButtonStyle.green, label=f'NUMBER OF REGISTERED PLAYERS : {total}', emoji='📝',
+                                custom_id='racing_count', disabled=False),
+                        ]
+                    ]
+                )
+                await interaction.channel.send("📢 คุณได้ลงทะเบียนไว้แล้ว", delete_after=5)
+                return
+        
+        elif event_btn == "racing_count":
+            await interaction.edit_origin(
+                    components=[
+                        [
+                            Button(style=ButtonStyle.blue, label='REGISTER', emoji='📝', custom_id='racing_register'),
+                            Button(style=ButtonStyle.green, label=f'NUMBER OF REGISTERED PLAYERS : {total}', emoji='📝',
+                                custom_id='racing_count', disabled=False),
+                        ]
+                    ]
+                )
     @commands.command(name='racing')
     async def racing_command(self, ctx):
         total = count_event_player()
@@ -39,7 +65,7 @@ class RacingEvent(commands.Cog):
                 [
                     Button(style=ButtonStyle.blue, label='REGISTER', emoji='📝', custom_id='racing_register'),
                     Button(style=ButtonStyle.red, label=f'NUMBER OF REGISTERED PLAYERS : {total}', emoji='📝',
-                           custom_id='racing_count', disabled=True),
+                           custom_id='racing_count', disabled=False),
                 ]
             ]
         )
